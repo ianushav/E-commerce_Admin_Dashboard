ae900274f741642667318a1cca9a72a4270d2372
Anusha <anushav259@gmail.com>
2025-03-17 22:17:25 +0530

    Issues solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { StrictMode } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
@@ -14,23 +14,19 @@ import UpdateProducts from "./Components/UpdateProducts";
 function App() {
   return (
     <StrictMode>
-      <Router basename="/E-commerce_Admin_Dashboard">
+      <Router>
         <AdminNavbar />
         <div className="d-flex flex-md-row">
           {/* Sidebar */}
           <AdminSidebar />
           <div className="flex-grow-1 p-3">
             <Routes>
-              {/* Default Route (Redirect to /dashboard) */}
-              <Route path="/" element={<Navigate to="/dashboard" />} />
-              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/" element={<Dashboard />} />
               <Route path="/customers" element={<AdminCustomers />} />
               <Route path="/orders" element={<AdminOrders />} />
               <Route path="/products" element={<AdminProducts />} />
               <Route path="/addproducts" element={<AddProducts />} />
               <Route path="/updateproducts" element={<UpdateProducts />} />
-              {/* Catch-all for Undefined Routes */}
-              <Route path="*" element={<h2>404 Not Found</h2>} />
             </Routes>
           </div>
         </div>
